56efb0606455dedd8a04757fa55fa9c1ecaa8837
Justin Loh <justinlohkingwei@gmail.com>
2021-10-06 00:33:15 -0400

    Complete Change of WEBGL component implementation

--- src/components/WebglBox.tsx
+++ src/components/WebglBox.tsx
@@ -1,27 +1,35 @@
-import init from "../../pages/api/webgl/init";
 import styled from "styled-components";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  loaderUrl: "build/myunityapp.loader.js",
+  dataUrl: "build/myunityapp.data",
+  frameworkUrl: "build/myunityapp.framework.js",
+  codeUrl: "build/myunityapp.wasm",
+});
 
 const Box = styled.div`
-background: #d9d9d9;
-width: 800px;
-height: 450px;
-margin-bottom: 2rem;
-margin-top: 4rem;
-`;
-
-const WebGLCanvas = styled.canvas`
-background: #9bad9a;
-width: inherit;
-height: inherit;
+  background: #d9d9d9;
+  width: 800px;
+  height: 450px;
+  margin-bottom: 2rem;
+  margin-top: 4rem;
 `;
 
 const WebglBox = (): JSX.Element => {
-  const componentDidMount = () => {
-    init(`webgl`);
-  };
+  function handleOnClickFullscreen() {
+    unityContext.setFullscreen(true);
+  }
   return (
     <Box>
-      <WebGLCanvas id="webgl" onMouseOver={componentDidMount}/>
+      <Unity
+        unityContext={unityContext}
+        style={{
+          height: "inheirit",
+          width: "inheirit",
+          background: "#9a2323",
+        }}
+      />
     </Box>
   );
 };
