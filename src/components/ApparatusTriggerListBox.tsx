e27bd70452452d403d6abf07523f8a342fd03cde
Sean Mckenzie <60203505+mckenzietsean@users.noreply.github.com>
2021-11-05 16:00:13 -0400

    Removed unused ExpandMoreIcon

--- src/components/ApparatusTriggerListBox.tsx
+++ src/components/ApparatusTriggerListBox.tsx
@@ -8,7 +8,6 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import { useState } from "react";
 import { Button } from "@material-ui/core";
