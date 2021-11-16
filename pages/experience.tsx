239fef756ca21d670b9bf6058444c7a700a5917f
Justin Loh <justinlohkingwei@gmail.com>
2021-11-15 21:57:59 -0500

    Deleted some unused imports and took care of eslint problems

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,11 +1,6 @@
 import styled from "styled-components";
-import ApparatusListBox from "../src/components/apparatusLists/ApparatusListBox";
-import ApparatusSelectedListBox from "../src/components/apparatusLists/ActionSequenceBox";
-import ApparatusTriggerListBox from "../src/components/apparatusLists/ApparatusTriggerListBox";
 import NavigationBox from "../src/components/NavigationBox";
 import WebglBox from "../src/components/webgl/WebglBox";
-import Getimage from "../src/components/Getimage";
-
 import wobbleSphere from "../src/data/wobble-sphere.json";
 import evilCylinder from "../src/data/evil-cylinder.json";
 
@@ -20,7 +15,7 @@ const Content = styled.div`
 // TEMPORARY FUNCTION
 // REPLACE WHEN WE LOAD FROM AZURE DB
 const loadJSON = (id) => {
-    if(id == "wobble-sphere")
+    if(id === "wobble-sphere")
       return wobbleSphere;
 
     return evilCylinder;
@@ -38,8 +33,6 @@ const Experience = ({id}): JSX.Element => (
   </main>
 );
 
-Experience.getInitialProps = ({ query: { id } }) => {
-  return { id };
-}
+Experience.getInitialProps = ({ query: { id } }) => ({ id })
 
 export default Experience;
