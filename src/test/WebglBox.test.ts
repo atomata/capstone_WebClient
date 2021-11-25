26c65d84c2039892c0d1647f5c8176dbb967d1c6
deepbluecanada <deepbluecanada@deepbluecanadadeMacBook-Pro.local>
2021-11-24 19:10:30 -0500

    function test for getjsonfromurl and remove useless test

--- src/test/WebglBox.test.ts
+++ src/test/WebglBox.test.ts
@@ -2,6 +2,6 @@ import {render, screen} from "@testing-library/react"
 import React from 'react';
 import WebglBox from '../components/webgl/WebglBox'
 
-test('ApparatusSelectedListBox  renders without crashing', ()=>{
+test('ApparatusSelectedListBox renders without crashing', ()=>{
     render(<WebglBox/>);
 })
--- src/test/getjson.test.ts
+++ src/test/getjson.test.ts
@@ -1,8 +1,14 @@
 import getjsonfromurl from '../../api/getjson/getjson';
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ test: 100 }),
+  }),
+) as jest.Mock;
 
 test('based on the url, it should return a json object', () => {
     const id = 'fake-data-1'
     const output = "object";
     const testoutput = typeof getjsonfromurl(id)
+    
 })
