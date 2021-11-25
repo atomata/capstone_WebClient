f39aaecbcf0afd5728b53e9ac4ba170fcde8540e
deepbluecanada <deepbluecanada@deepbluecanadadeMacBook-Pro.local>
2021-11-25 10:30:57 -0500

    remove code smell

--- src/test/getjson.test.tsx
+++ src/test/getjson.test.tsx
@@ -10,5 +10,6 @@ test('based on the url, it should return a json object', () => {
     const id = 'fake-data-1'
     const output = "object";
     const testoutput = typeof getjsonfromurl(id)
+    expect(testoutput).toBe(output)
     
 })
