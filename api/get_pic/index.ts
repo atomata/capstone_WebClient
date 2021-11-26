1b68a0dcd081fe088c536b8799f925a1e551fb36
deepbluecanada <deepbluecanada@deepbluecanadadeMacBook-Pro.local>
2021-11-26 11:30:00 -0500

    remove code smell

--- api/get_pic/index.ts
+++ api/get_pic/index.ts
@@ -1,4 +1,4 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context} from "@azure/functions"
 import {
     BlobDownloadResponseParsed,
     BlobServiceClient
