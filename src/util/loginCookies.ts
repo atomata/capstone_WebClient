509f0ee9121958ff5a7edcfadfa7d8b0ff81ffe5
Sean Mckenzie <60203505+mckenzietsean@users.noreply.github.com>
2022-01-14 15:32:41 -0500

    Fixed Bug in Code Structure

--- src/util/loginCookies.ts
+++ src/util/loginCookies.ts
@@ -28,11 +28,8 @@ function getUserName(){
 
 // Logs user in with given param username
 function logIn(username){
-    // We're testing on localhost
-    if(document.location.host === "localhost:3000")
-        cookies.set('user', username, {domain: window.location.hostname, path: '/', secure: true, sameSite: 'none'});
-    else
-        cookies.set('user', username, {domain: window.location.hostname, path: '/', secure: true, sameSite: 'none'});
+
+    cookies.set('user', username, {domain: window.location.hostname, path: '/', secure: true, sameSite: 'none'});
 
     console.log(document.location.host);
     console.log(window.location.hostname);
