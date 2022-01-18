83c4946f0168ca2135ae2339308710b48563d40d
Yasaman <yasamanarya80@yahoo.com>
2022-01-18 16:56:31 -0500

    updated  unitycontextactions for the new container

--- src/util/unityContextActions.ts
+++ src/util/unityContextActions.ts
@@ -1,20 +1,19 @@
 import { UnityContext } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
-  // loaderUrl: "build/Builds.loader.js",
-  // dataUrl: "build/Builds.data",
-  // frameworkUrl: "build/Builds.framework.js",
-  // codeUrl: "build/Builds.wasm",
+  loaderUrl: "build/Builds.loader.js",
+  dataUrl: "build/Builds.data",
+  frameworkUrl: "build/Builds.framework.js",
+  codeUrl: "build/Builds.wasm",
 
-  loaderUrl: "build/0_0_2.loader.js",
-  dataUrl: "build/0_0_2.data",
-  frameworkUrl: "build/0_0_2.framework.js",
-  codeUrl: "build/0_0_2.wasm",
+  // loaderUrl: "build/0_0_2.loader.js",
+  // dataUrl: "build/0_0_2.data",
+  // frameworkUrl: "build/0_0_2.framework.js",
+  // codeUrl: "build/0_0_2.wasm",
 });
 
 // TODO what if args string is empty?
 function load(arg: string) {
-  console.log("Hello", arg);
   unityContext.send("AtomataScene", "LoadApparatus", arg);
 }
 
@@ -23,10 +22,18 @@ function voidTrigger(arg: string) {
   unityContext.send("AtomataScene", "Trigger", arg);
 }
 
+function defaultCameraView() {
+  unityContext.send("AtomataScene", "ReturnCamera");
+}
+
 // TODO what if args string is empty?
 function callToWebGL(path: string, input: string) {
-  const arg = `${path}@${input}`;
+  const arg = `${path}/${input}`;
   voidTrigger(arg);
 }
 
-export { callToWebGL, load, voidTrigger, unityContext };
+function changeSkybox(skybox : string) {
+  unityContext.send("AtomataScene", "LoadSkybox", skybox)
+}
+
+export { callToWebGL, load, voidTrigger, defaultCameraView,changeSkybox, unityContext };
