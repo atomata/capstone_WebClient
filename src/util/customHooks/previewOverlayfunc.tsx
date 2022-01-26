c36ff21605b780f037266ffc6a84082eba3fff95
Yasaman <yasamanarya80@yahoo.com>
2022-01-26 13:27:15 -0500

    enhanced preview overlay custom hook

--- src/util/customHooks/previewOverlayfunc.tsx
+++ src/util/customHooks/previewOverlayfunc.tsx
@@ -7,33 +7,31 @@ const useSelected = (
 ): {
   selected: number;
   setSelected: Dispatch<SetStateAction<number>>;
-  cyclePreviewLeft: () => () => void;
-  cyclePreviewRight: () => () => void;
+  cyclePreviewLeft: () => void;
+  cyclePreviewRight: () => void;
 } => {
   const [selected, setSelected] = useState(0);
 
-  function cyclePreviewRight() {
-    return selected < actionList.length - 1
-      ? () => {
-          setSelected(selected + 1);
-          requestTrigger(actionList[selected+1].path, actionList[selected+1].input);
-        }
-      : null;
-    // same logic with the cyclePreviewLeft, but check if seleteced is the end of the actionList
-  }
-
-  function cyclePreviewLeft() {
-    if (selected > 0) {
-      return () => {
-        setSelected(selected - 1);
-        requestTrigger(actionList[selected-1].path, actionList[selected-1].input);
-      };
+  const cyclePreviewRight = () => {
+    if (selected < actionList.length - 1) {
+      requestTrigger(
+        actionList[selected + 1].path,
+        actionList[selected + 1].input
+      );
+      setSelected((prevVal) => prevVal + 1);
     }
-    return () => {
-      setSelected(selected);
-      requestTrigger(actionList[selected].path, actionList[selected].input);
-    };
-  }
+  };
+  // same logic with the cyclePreviewLeft, but check if seleteced is the end of the actionList
+
+  const cyclePreviewLeft = () => {
+    if (selected > 0) {
+      requestTrigger(
+        actionList[selected - 1].path,
+        actionList[selected - 1].input
+      );
+      setSelected((prevVal) => prevVal - 1);
+    }
+  };
 
   return { selected, setSelected, cyclePreviewLeft, cyclePreviewRight };
 };
