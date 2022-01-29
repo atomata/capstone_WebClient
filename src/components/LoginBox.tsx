db46e12dc9d79a2bcda2941b79b8ac43f38d4176
Tashfia <46882997+tashfia98@users.noreply.github.com>
2022-01-29 00:54:33 -0500

    login page enchancement
    -attempt to make like the mockup

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -1,35 +1,81 @@
 import styled from "styled-components";
 import { Button, TextField } from "@material-ui/core";
 import { useLoginSubmit } from "../util/customHooks/loginBoxfunc";
+import { urlFor } from "../util/utils";
+import { makeStyles } from "@material-ui/core/styles";
 
 const LoginRoot = styled.div`
-  margin-bottom: 2rem;
-  margin-top: 4rem;
-  justify-content: center;
-  flex-direction: column;
-  justify-items: center;
+  position: relative;
+  width: inherit;
+  display: flex;
+  background-color: #A5A4EA;
 `;
 
 const LoginContainer = styled.div`
-  height: 500px;
-  width: 500px;
   text-align: center;
-  padding: 15px;
-  background-color: grey;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  background: #3F3D56;
+  width: 50%;
+  margin-left: 50%;
 `;
 
 const LoginFields = styled.div`
-  margin-top: 65px;
-  padding: 15px;
+  margin: 15px;
 `;
 
 const LoginButton = styled(Button)`
   && {
-    background-color: #5dc0cb;
+    background-color: #FFFFFF;
     display: block-inline;
-    margin-top: 45px;
+    color: #1710A1;
+    margin-top: 30px;
+    padding: 15px;
+    padding-left: 85px;
+    padding-right: 85px;
+    font-weight: bold;
   }
 `;
+
+const ImgIllustration = styled.img.attrs({
+  alt: "Image Placeholder",
+})
+  `
+  @media only screen and (max-width: 1130px) {
+    width: 52%;
+  }
+
+  @media only screen and (max-width: 700px) {
+    width: 48%;
+  }
+
+  width: 62%;
+  position: absolute;
+  display:block;
+  height: 600px;
+  margin-top: 5%;
+  margin-bottom: 5%;
+  margin-left:50px;
+  `
+;
+
+const ImgLogo = styled.img.attrs({
+  alt: "Image Placeholder",
+})
+  `
+  display:block;
+  height: 30%;
+  margin-left: auto;
+  margin-right: auto;
+  `
+;
+
+const useStyles = makeStyles(theme => ({
+  textField: {
+      color: "#FFFFFF"
+  }
+}));
+
 
 function LoginBox(): JSX.Element {
   const {
@@ -38,12 +84,14 @@ function LoginBox(): JSX.Element {
     handleSubmit
   } = useLoginSubmit();
 
+  const classes = useStyles();
+
   // https://mui.com/components/text-fields/
   return (
     <LoginRoot>
       <LoginContainer>
+      <ImgLogo src={urlFor("assets/epistaLogo.png")} />
         <form className="loginForm" autoComplete="off" onSubmit={handleSubmit}>
-          <h1>LOG IN</h1>
           <LoginFields>
             <TextField
               required
@@ -51,7 +99,15 @@ function LoginBox(): JSX.Element {
               id="username-input"
               label="Username"
               helperText={nameErr}
+              variant="outlined" 
+              InputProps={{
+                className: classes.textField
+              }}
+              InputLabelProps={{
+                className: classes.textField
+              }}
             />
+            <br />
             <br />
             <TextField
               required
@@ -61,12 +117,20 @@ function LoginBox(): JSX.Element {
               type="password"
               autoComplete="current-password"
               helperText={passErr}
+              variant="outlined" 
+              InputProps={{
+                className: classes.textField
+              }}
+              InputLabelProps={{
+                className: classes.textField
+              }}
             />
             <br />
-            <LoginButton type="submit" variant="contained">Log In</LoginButton>
+            <LoginButton type="submit" variant="contained">LOG IN</LoginButton>
           </LoginFields>
         </form>
       </LoginContainer>
+      <ImgIllustration src={urlFor("assets/loginillustration.svg")} />
     </LoginRoot>
   );
 }
