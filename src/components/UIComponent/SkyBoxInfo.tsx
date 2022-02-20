6c6cc55b71621c22368cc63ac04b4cb12ed9e617
shushengfuqin <ruocjia@gmail.com>
2022-02-19 19:45:25 -0500

    reformat skyboxinfo.tsx

--- src/components/UIComponent/SkyBoxInfo.tsx
+++ src/components/UIComponent/SkyBoxInfo.tsx
@@ -11,7 +11,10 @@ import {
   MenuItem,
 } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { changeSkybox, defaultCameraView } from "../../util/unityContextActions";
+import {
+  changeSkybox,
+  defaultCameraView,
+} from "../../util/unityContextActions";
 
 function SkyBoxInfo(): JSX.Element {
   const ITEM_HEIGHT = 48;
@@ -35,46 +38,48 @@ function SkyBoxInfo(): JSX.Element {
   };
   return (
     <>
-      <button type ="button"
-              aria-label="more"
-              id="long-button"
-              aria-controls={open ? "long-menu" : undefined}
-              aria-expanded={open ? "true" : undefined}
-              aria-haspopup="true"
-              onClick={handleClick}
-            >Change Sky Box
-              <MoreVertIcon />
-            </button>
+      <button
+        type="button"
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? "long-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Change Sky Box
+        <MoreVertIcon />
+      </button>
       <Menu
-              id="long-menu"
-              MenuListProps={{
-                "aria-labelledby": "long-button",
-              }}
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              PaperProps={{
-                style: {
-                  maxHeight: ITEM_HEIGHT * 4.5,
-                  width: "20ch",
-                },
-              }}
-            >
-              {skyboxList.map((skybox) => (
-                <MenuItem
-                  key={skybox}
-                  onClick={() => {
-                    handleClose();
-                    changeSkybox(skybox);
-                  }}
-                >
-                  {skybox}
-                </MenuItem>
-              ))}
-            </Menu>
-            <button type="button" key="cam_view" onClick={() => defaultCameraView()}>
-              Default View
-            </button>
+        id="long-menu"
+        MenuListProps={{
+          "aria-labelledby": "long-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: "20ch",
+          },
+        }}
+      >
+        {skyboxList.map((skybox) => (
+          <MenuItem
+            key={skybox}
+            onClick={() => {
+              handleClose();
+              changeSkybox(skybox);
+            }}
+          >;
+            {skybox}
+          </MenuItem>
+        ))}
+      </Menu>
+      <button type="button" key="cam_view" onClick={() => defaultCameraView()}>
+        Default View
+      </button>
     </>
   );
 }
