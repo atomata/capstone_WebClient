33a3d98b9a27318809710e30237320c853d4a3bb
Yasaman <yasamanarya80@yahoo.com>
2022-03-16 23:29:23 -0400

    added missing tests for overlay + fixed the test failings

--- src/test/overlay.test.tsx
+++ src/test/overlay.test.tsx
@@ -3,23 +3,39 @@ import { render } from "@testing-library/react";
 import { act, renderHook } from "@testing-library/react-hooks";
 import Overlay from "../components/Editor/Overlay";
 import { useActionList, useOverlay } from "../util/customHooks/overlayfunc";
-import { ActionData } from "../util/types";
+import { ActionData, ExperienceData } from "../util/types";
+import { useActionBar } from "../util/customHooks/ActionBarFunc";
+import {
+  globalContextTypes,
+  GlobalContext,
+} from "../util/customHooks/globalContext";
+import {testExperienceData} from "../util/testConstants";
+
 
 const actionData1: ActionData = {
   path: "test1",
-  input: {command:"testinput1", name:"", desc:""},
+  input: { command: "testinput1", name: "", desc: "" },
   assetId: "testasset1",
   desc: "This is the text added to action data in text box",
 };
 const actionData2: ActionData = {
   path: "test2",
-  input: {command:"testinput2", name:"", desc:""},
+  input: { command: "testinput2", name: "", desc: "" },
   assetId: "testasset2",
   desc: "This is the text added to action data in text box",
 };
 
 test("Overlay renders without crashing", () => {
-  render(<Overlay />);
+  const globalContextValues: globalContextTypes = {
+    experienceData : testExperienceData,
+    setExperienceData: ()=> undefined,
+    userId:"testuser1",
+  };
+  render(
+    <GlobalContext.Provider value={globalContextValues}>
+      <Overlay />
+    </GlobalContext.Provider>
+  );
 });
 
 describe("showOverlay", () => {
@@ -90,4 +106,95 @@ describe("ActionList", () => {
       expect.arrayContaining([actionData1])
     );
   });
+});
+
+describe("ActionBar", () => {
+  it("ToggleTextBox", () => {
+    const { result } = renderHook(useActionBar);
+    act(() => {
+      result.current.toggleTextBox();
+    });
+    expect(result.current.textBox).toEqual(true);
+  });
+
+  it("ToggleSavingTip", () => {
+    const { result } = renderHook(useActionBar);
+    act(() => {
+      result.current.toggleSavingTip();
+    });
+    expect(result.current.savingTip).toEqual(true);
+  });
+
+  it("ToggleSkybox 1 ", () => {
+    const { result } = renderHook(useActionBar);
+    act(() => {
+      result.current.toggleToolDoc();
+    });
+    act(() => {
+      result.current.toggleSkyBoxInfo();
+    });
+    expect(result.current.skyBoxInfo).toEqual(true);
+  });
+
+  it("ToggleSkybox 2 ", () => {
+    const { result } = renderHook(useActionBar);
+    act(() => {
+      result.current.toggleSkyBoxInfo();
+    });
+    expect(result.current.skyBoxInfo).toEqual(true);
+  });
+
+  it("ToggleSkybox 3 ", () => {
+    const { result } = renderHook(useActionBar);
+    act(() => {
+      result.current.toggleSkyBoxInfo();
+    });
+    act(() => {
+      result.current.toggleSkyBoxInfo();
+    });
+    expect(result.current.skyBoxInfo).toEqual(false);
+  });
+
+  it("ToggleToolDoc", () => {
+    const { result } = renderHook(useActionBar);
+    act(() => {
+      result.current.toggleToolDoc();
+    });
+    expect(result.current.toolDoc).toEqual(false);
+  });
+
+  it("ToggleApparatusInfo 1", () => {
+    const { result } = renderHook(useActionBar);
+    act(() => {
+      result.current.toggleToolDoc();
+    });
+    act(() => {
+      result.current.toggleApparatusInfo();
+    });
+    expect(result.current.apparatusInfo).toEqual(true);
+  });
+
+
+  it("ToggleApparatusInfo 2", () => {
+    const { result } = renderHook(useActionBar);
+    act(() => {
+      result.current.toggleApparatusInfo();
+    });
+    expect(result.current.apparatusInfo).toEqual(false);
+  });
+
+  it("ToggleApparatusInfo 3", () => {
+    const { result } = renderHook(useActionBar);
+    act(() => {
+      result.current.toggleApparatusInfo();
+    });
+    act(() => {
+      result.current.toggleToolDoc();
+    });
+    act(() => {
+      result.current.toggleApparatusInfo();
+    });
+    expect(result.current.apparatusInfo).toEqual(true);
+  });
+
 });
