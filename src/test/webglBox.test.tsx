131f849c7f75f802a646f0f718594511c4df459e
Yasaman <yasamanarya80@yahoo.com>
2022-03-16 23:28:45 -0400

    test for webglbox component

--- src/test/webglBox.test.tsx
+++ src/test/webglBox.test.tsx
@@ -1,8 +1,19 @@
 import { render } from "@testing-library/react";
 import React from "react";
 import WebglBox from "../components/Editor/WebglBox";
+import {GlobalContext, globalContextTypes} from "../util/customHooks/globalContext";
+import {testExperienceData} from "../util/testConstants";
 
 
-test("ApparatusSelectedListBox renders without crashing", () => {
-  render(<WebglBox />);
+test("WebglBox renders without crashing", () => {
+  const globalContextValues: globalContextTypes = {
+    experienceData : testExperienceData,
+    setExperienceData: ()=> undefined,
+    userId:"testuser1",
+  };
+  render(
+      <GlobalContext.Provider value={globalContextValues}>
+        <WebglBox />
+      </GlobalContext.Provider>
+  );
 });
