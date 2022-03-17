d75a3bcedeae5d9c1e232c13a353ce0104c70964
Yasaman <yasamanarya80@yahoo.com>
2022-03-16 23:27:09 -0400

    enhanced side bar components test

--- src/test/skyboxInfo.test.tsx
+++ src/test/skyboxInfo.test.tsx
@@ -1,12 +1,9 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import CategoryIcon from "@mui/icons-material/Category";
-import AssetItem from "../components/Editor/TreeView/AssetItem";
+import SkyBoxInfo from "../components/Editor/SideBar/SkyBoxInfo";
 
-test("ActionItem renders without crashing", () => {
-  render(<AssetItem labelText="TestItem" nodeId="Test" LabelIcon={CategoryIcon} />);
+test("Skybox renders without crashing", () => {
+  render(<SkyBoxInfo/>);
 });
 
-test("ActionItem renders without crashing with undefined LabelText", () => {
-  render(<AssetItem labelText={undefined} nodeId="Test" LabelIcon={CategoryIcon} />);
-});
+
