f435b132d3a03e7be91c225dc05ed90a7abb2076
Yasaman <yasamanarya80@yahoo.com>
2022-03-17 21:54:57 -0400

    added minimum size for textbox

--- src/components/Editor/TextEditor.tsx
+++ src/components/Editor/TextEditor.tsx
@@ -3,35 +3,38 @@ import React, { useContext, useEffect, useState } from "react";
 import { Box, IconButton } from "@mui/material";
 import SaveAltIcon from "@mui/icons-material/SaveAlt";
 import ExpandIcon from "@mui/icons-material/Expand";
+import styles from "../SideBarButtons.module.css";
 import { ActionContext } from "../../util/customHooks/actionContext";
 
-const Header = styled.div`
-  color: white;
-`;
+// const Header = styled.div`
+//   color: white;
+// `;
 
 const TextDiv = styled.div`
   background: #3f3d56;
   display: flex;
   width: inherit;
   height: inherit;
+  min-width: 10rem;
+  min-height: 10rem;
   flex-direction: column;
   border-radius: 15px;
 `;
 
-const DefaultButton = styled.div`
-  padding: 0.5em;
-  cursor: pointer;
-  text-align: center;
-  font-family: Inter, monospace;
-  font-size: 1.5em;
-  color: white;
-  background: #3f3d56;
-  max-height: 22%;
-  border-radius: 15px;
-`;
+// const DefaultButton = styled.div`
+//   padding: 0.5em;
+//   cursor: pointer;
+//   text-align: center;
+//   font-family: Inter, monospace;
+//   font-size: 1.5em;
+//   color: white;
+//   background: #3f3d56;
+//   max-height: 22%;
+//   border-radius: 15px;
+// `;
 
 const TextEditor = (): JSX.Element => {
-  const [isExpanded, setExpanded] = useState(false);
+  // const [isExpanded, setExpanded] = useState(false);
   const [currDesc, setCurrDesc] = useState("");
   const { actionList, selectedAction, setDescription } =
     useContext(ActionContext);
@@ -42,24 +45,24 @@ const TextEditor = (): JSX.Element => {
       setCurrDesc("");
     }
   }, [selectedAction, actionList]);
-  return isExpanded ? (
+  return (
     <TextDiv>
-      <Header>
+      <div>
+        {/*<IconButton*/}
+        {/*  sx={{ color: "white" }}*/}
+        {/*  onClick={() => {*/}
+        {/*    setExpanded((value) => !value);*/}
+        {/*  }}*/}
+        {/*>*/}
+        {/*  <ExpandIcon sx={{ fontSize: 30, ml: "0.5em" }} />*/}
+        {/*</IconButton>*/}
         <IconButton
-          sx={{ color: "white" }}
-          onClick={() => {
-            setExpanded((value) => !value);
-          }}
-        >
-          <ExpandIcon sx={{ fontSize: 30, ml: "0.5em" }} />
-        </IconButton>
-        <IconButton
-          sx={{ color: "white" }}
+          className={styles.sidebarItem}
           onClick={() => setDescription(currDesc)}
         >
-          <SaveAltIcon sx={{ fontSize: 30 }} />
+          <SaveAltIcon sx={{ fontSize: 30, ml: "0.3rem" }} />
         </IconButton>
-      </Header>
+      </div>
       <Box
         sx={{
           m: "1em",
@@ -71,24 +74,27 @@ const TextEditor = (): JSX.Element => {
         }}
       >
         <textarea
+          draggable="false"
           style={{
             background: "white",
             borderRadius: "5px",
             width: "100%",
             height: "100%",
+            minHeight: "5rem",
             resize: "none",
-            fontSize: "1.6em",
+            fontSize: "1.6rem",
           }}
           value={currDesc}
           onChange={(e) => setCurrDesc(e.target.value)}
         />
       </Box>
     </TextDiv>
-  ) : (
-    <DefaultButton onClick={() => setExpanded((value) => !value)}>
-      ADD/EDIT DESCRIPTION
-    </DefaultButton>
   );
+  //     : (
+  //   <DefaultButton onClick={() => setExpanded((value) => !value)}>
+  //     ADD/EDIT DESCRIPTION
+  //   </DefaultButton>
+  // );
 };
 
 export default TextEditor;
