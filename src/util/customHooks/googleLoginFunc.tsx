fd7c69a0c818d021028a805afe45ed0bcfc9fa68
deepbluecanada <deepbluecanada@deepbluecanadadeMacBook-Pro.local>
2022-03-28 13:03:43 -0400

    remove code smell

--- src/util/customHooks/googleLoginFunc.tsx
+++ src/util/customHooks/googleLoginFunc.tsx
@@ -3,11 +3,9 @@ import { refreshTokenSetup } from "../../components/GoogleLog/refreshTokenSetup"
 import { logIn } from "../loginCookies";
 
 const useGoogleLog = () => {
-    const [name, setName] = useState<string>();
   const onSuccess = (res) => {
     console.log("[Login Success] currentUser: ", res.progileObj);
     alert(`Logged in successfully welcome ${res.profileObj.name}`);
-    setName(`${res.profileObj.googleId}`);
     logIn(`${res.profileObj.googleId}`);
     refreshTokenSetup(res);
   };
