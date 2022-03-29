20d4bceff26492629fd7823f59a1e2345d8c150d
Yasaman <yasamanarya80@yahoo.com>
2022-03-28 23:07:15 -0400

    added two unit test for guidefunc

--- src/test/guide.test.tsx
+++ src/test/guide.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import React, { useContext } from "react";
+import React from "react";
 import { act, renderHook } from "@testing-library/react-hooks";
 import { GlobalContext, globalContextTypes } from "../util/customHooks/globalContext";
 import { ActionContext } from "../util/customHooks/actionContext";
@@ -7,6 +7,7 @@ import {testExperienceData} from "../util/testConstants";
 import { useGuide } from "../util/customHooks/guideFunc";
 import Guide from "../components/Editor/Guide";
 import { SideBarContext, sideBarContextType } from "../util/customHooks/SideBarContext";
+import {useActionBar} from "../util/customHooks/ActionBarFunc";
 
 const sideBarContextValues: sideBarContextType = {
   toggleTextBox : () => undefined,
@@ -45,11 +46,46 @@ test("Guide renders without crashing", () => {
 });
 
 describe("Guide Funcs", () => {
+
+  const { result } = renderHook(() =>useActionBar());
+  const sideBarContextValuesTest = {
+    toggleTextBox : result.current.toggleTextBox,
+    toggleToolDoc: result.current.toggleToolDoc,
+    toggleApparatusInfo: result.current.toggleApparatusInfo,
+    toggleSkyBoxInfo : result.current.toggleSkyBoxInfo,
+    toggleOverlay : () => undefined,
+    toggleGuide : result.current.toggleGuide,
+    setGuideNum :  result.current.setGuideNum,
+    toggleSavingTip: () => undefined,
+    toolDoc: false,
+    apparatusInfo: false,
+    skyBoxInfo: false,
+    showOverlay: false,
+    savingTip: false,
+    showGuide: false,
+    guideNum: result.current.guideNum
+  }
+
+  const wrapper = ({ children }) => (
+      <SideBarContext.Provider value={sideBarContextValuesTest}>{children}</SideBarContext.Provider>
+  )
   it("Next Page", () => {
-    const { result } = renderHook(useGuide);
+    const { result: result2 } = renderHook(() => useGuide(), { wrapper });
     act(() => {
-      result.current.nextPage();
+      result2.current.nextPage();
     });
-    expect(result.current.guideNum).toEqual(2);
+    expect(result.current.guideNum).toEqual(1);
+  });
+
+  it("Prev Page", () => {
+    const { result: result2 } = renderHook(() => useGuide(), { wrapper });
+    act(() => {
+      result2.current.nextPage();
+      result2.current.nextPage();
+    });
+    act(() => {
+      result2.current.prevPage();
+    });
+    expect(result.current.guideNum).toEqual(1);
   });
 });
