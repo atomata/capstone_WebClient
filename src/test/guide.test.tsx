0f48ae5821bea64715566af645407b4ab8d84067
Yasaman <yasamanarya80@yahoo.com>
2022-03-28 23:18:30 -0400

    cleanup

--- src/test/guide.test.tsx
+++ src/test/guide.test.tsx
@@ -1,49 +1,8 @@
-import { render } from "@testing-library/react";
 import React from "react";
 import { act, renderHook } from "@testing-library/react-hooks";
-import { GlobalContext, globalContextTypes } from "../util/customHooks/globalContext";
-import { ActionContext } from "../util/customHooks/actionContext";
-import {testExperienceData} from "../util/testConstants";
 import { useGuide } from "../util/customHooks/guideFunc";
-import Guide from "../components/Editor/Guide";
-import { SideBarContext, sideBarContextType } from "../util/customHooks/SideBarContext";
+import { SideBarContext } from "../util/customHooks/SideBarContext";
 import {useActionBar} from "../util/customHooks/ActionBarFunc";
-
-const sideBarContextValues: sideBarContextType = {
-  toggleTextBox : () => undefined,
-  toggleToolDoc: () => undefined,
-  toggleApparatusInfo: () => undefined,
-  toggleSkyBoxInfo : () => undefined,
-  toggleOverlay : () => undefined,
-  toggleGuide : () => undefined,
-  setGuideNum : () => undefined,
-  toggleSavingTip: () => undefined,
-  toolDoc: false,
-  apparatusInfo: false,
-  skyBoxInfo: false,
-  showOverlay: false,
-  savingTip: false,
-  showGuide: false,
-  guideNum: 0
-}
-
-test("Guide renders without crashing", () => {
-  const globalContextValues: globalContextTypes = {
-    experienceData : testExperienceData,
-    setExperienceData: ()=> undefined,
-    userId:"testuser1",
-  };
-
-  render(
-    <GlobalContext.Provider value={globalContextValues}>
-      <ActionContext.Provider value={{ addActionToList: () => undefined }}>
-        <SideBarContext.Provider value={sideBarContextValues}>
-          <Guide />
-        </SideBarContext.Provider>
-      </ActionContext.Provider>
-    </GlobalContext.Provider>
-  );
-});
 
 describe("Guide Funcs", () => {
 
