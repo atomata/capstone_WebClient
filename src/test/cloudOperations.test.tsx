24f7bdfda4565a6c56dd53ea035bfa09016de5d7
deepbluecanada <deepbluecanada@deepbluecanadadeMacBook-Pro.local>
2022-04-02 22:33:48 -0400

    cloudOperation

--- src/test/cloudOperations.test.tsx
+++ src/test/cloudOperations.test.tsx
@@ -18,17 +18,7 @@ test("based on the apparatus id it should return the apparatus json, but can rej
   });
 });
 
-test("based on the apparatus id it should return the apparatus json", () => {
-  global.fetch = jest.fn(() =>
-    Promise.resolve({
-      json: () => Promise.resolve(testmetadata1),
-    })
-  ) as jest.Mock;
-  const apparatusId = "evil-cylinder";
-  return getApparatusFromCloud(apparatusId).then((testoutput) => {
-    expect(testoutput).toEqual(testmetadata1);
-  });
-});
+
 
 test("based on the user id and exp id, it should return the experience json but can reject", () => {
   global.fetch = jest.fn(() => Promise.reject()) as jest.Mock;
@@ -88,24 +78,68 @@ test("delete an experience form cloud", () => {
   });
 });
 
-test("setup apparatus data", () => {
-  const experienceDataTemp: ExperienceData = {
-    apparatusRoot: convertPathDataToTree(testmetadata1),
-    experience: {
-      experienceId: "exp1",
-      apparatusId: "",
-      actionList: [],
-      skyboxId: "",
-    },
-  };
 
+describe("appratus json data", () => {
   global.fetch = jest.fn(() =>
-    Promise.resolve({
-      json: () => Promise.resolve(testmetadata1),
-    })
-  ) as jest.Mock;
-  const apparatusId = "evil-cylinder";
-  setupApparatusData(apparatusId, experienceDataTemp).then(() => {
-    expect(experienceDataTemp.apparatusRoot).toEqual(convertPathDataToTree(testmetadata1));
+  Promise.resolve({
+    json: () => Promise.resolve(testmetadata1),
+  })) as jest.Mock;
+
+  it("based on the apparatus id it should return the apparatus json", () => {
+    const apparatusId = "evil-cylinder";
+    return getApparatusFromCloud(apparatusId).then((testoutput) => {
+      expect(testoutput).toEqual(testmetadata1);
+    });
   });
-});
+  
+  it("setup apparatus data", () => {
+    const experienceDataTemp: ExperienceData = {
+      apparatusRoot: convertPathDataToTree(testmetadata1),
+      experience: {
+        experienceId: "exp1",
+        apparatusId: "",
+        actionList: [],
+        skyboxId: "",
+      },
+    };
+
+    const apparatusId = "evil-cylinder";
+    setupApparatusData(apparatusId, experienceDataTemp).then(() => {
+      expect(experienceDataTemp.apparatusRoot).toEqual(convertPathDataToTree(testmetadata1));
+    });
+  });
+  
+})
+// test("based on the apparatus id it should return the apparatus json", () => {
+//   global.fetch = jest.fn(() =>
+//     Promise.resolve({
+//       json: () => Promise.resolve(testmetadata1),
+//     })
+//   ) as jest.Mock;
+//   const apparatusId = "evil-cylinder";
+//   return getApparatusFromCloud(apparatusId).then((testoutput) => {
+//     expect(testoutput).toEqual(testmetadata1);
+//   });
+// });
+
+// test("setup apparatus data", () => {
+//   const experienceDataTemp: ExperienceData = {
+//     apparatusRoot: convertPathDataToTree(testmetadata1),
+//     experience: {
+//       experienceId: "exp1",
+//       apparatusId: "",
+//       actionList: [],
+//       skyboxId: "",
+//     },
+//   };
+
+//   global.fetch = jest.fn(() =>
+//     Promise.resolve({
+//       json: () => Promise.resolve(testmetadata1),
+//     })
+//   ) as jest.Mock;
+//   const apparatusId = "evil-cylinder";
+//   setupApparatusData(apparatusId, experienceDataTemp).then(() => {
+//     expect(experienceDataTemp.apparatusRoot).toEqual(convertPathDataToTree(testmetadata1));
+//   });
+// });
